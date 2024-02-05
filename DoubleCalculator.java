cfd612e9de9166585d6c3e7db10204a65752b79b
AfnanSSa <iafnanz67@gmail.com>
2024-02-05 12:36:34 +0400

    Added the Double Calculator

---
+++ DoubleCalculator.java
@@ -0,0 +1,21 @@
+//this is Double Calculater which appplies the main mathematical operations on double value.
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        double firstNum = 10.5;
+        double secondNum = 20.5;
+		
+		//Addition
+        System.out.println("Addition: " + (firstNum + secondNum));
+		
+		//Subtraction
+        System.out.println("Subtraction: " + (secondNum - firstNum));
+		
+		//Multiplication
+        System.out.println("Multiplication: " + (firstNum * secondNum));
+		
+		//Division
+        System.out.println("Division: " + (firstNum/secondNum));
+    }
+}
