d292fc0cc9b3832ca6ab8f99496a682e274c15ee
AfnanSSa <iafnanz67@gmail.com>
2024-02-05 12:46:04 +0400

    Added the Short calculator

---
+++ ShortCalculator.java
@@ -0,0 +1,21 @@
+//this is Short values Calculator which appplies the main mathematical operations on short value.
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        short firstNum = 7;
+        short secondNum = 14;
+		
+		//Addition
+        System.out.println("Addition: " + (firstNum + secondNum));
+		
+		//Subtraction
+        System.out.println("Subtraction: " + (secondNum - firstNum));
+		
+		//Multiplication
+        System.out.println("Multiplication: " + (firstNum * secondNum));
+		
+		//Division
+        System.out.println("Division: " + (firstNum/secondNum));
+    }
+}
