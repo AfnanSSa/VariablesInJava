4298e9c765205ac95041404df38333ac0facdc85
AfnanSSa <iafnanz67@gmail.com>
2024-02-05 12:49:09 +0400

    Added the Float calculator

---
+++ FloatCalculator.java
@@ -0,0 +1,21 @@
+//this is Float values Calculator which appplies the main mathematical operations on float values.
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        float firstNum = 7.5f;
+        float secondNum = 14.7f;
+		
+		//Addition
+        System.out.println("Addition: " + (firstNum + secondNum));
+		
+		//Subtraction
+        System.out.println("Subtraction: " + (secondNum - firstNum));
+		
+		//Multiplication
+        System.out.println("Multiplication: " + (firstNum * secondNum));
+		
+		//Division
+        System.out.println("Division: " + (firstNum/secondNum));
+    }
+}
