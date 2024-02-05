b089611f8da09672faac705071c0d930cf4ee13a
AfnanSSa <iafnanz67@gmail.com>
2024-02-05 12:31:25 +0400

    Added the Integer Calculator

---
+++ IntegerCalculator.java
@@ -0,0 +1,21 @@
+//this is Integer Calculater which appplies the main mathematical operations on integers.
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int firstNum = 10;
+        int secondNum = 20;
+		
+		//Addition
+        System.out.println("Addition: " + (firstNum + secondNum));
+		
+		//Subtraction
+        System.out.println("Subtraction: " + (secondNum - firstNum));
+		
+		//Multiplication
+        System.out.println("Multiplication: " + (firstNum * secondNum));
+		
+		//Division
+        System.out.println("Division: " + (firstNum/secondNum));
+    }
+}
