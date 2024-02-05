07e4cdfb5425d05abb75baf97cc1e0395e48364f
AfnanSSa <iafnanz67@gmail.com>
2024-02-05 12:53:46 +0400

    Added the Long calculator

---
+++ LongCalculator.java
@@ -0,0 +1,21 @@
+//this is Long values Calculator which appplies the main mathematical operations on long values.
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        long firstNum = 7L;
+        long secondNum = 14L;
+		
+		//Addition
+        System.out.println("Addition: " + (firstNum + secondNum));
+		
+		//Subtraction
+        System.out.println("Subtraction: " + (secondNum - firstNum));
+		
+		//Multiplication
+        System.out.println("Multiplication: " + (firstNum * secondNum));
+		
+		//Division
+        System.out.println("Division: " + (firstNum/secondNum));
+    }
+}
